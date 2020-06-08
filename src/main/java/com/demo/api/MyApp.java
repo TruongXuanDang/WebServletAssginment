8a9428b548c136c707e2f4207a20853c5dc1bf76
TruongDX8 <truonghusk17@gmail.com>
2020-06-09 05:47:52 +0700

    get value of product list

--- src/main/java/com/demo/api/MyApp.java
+++ src/main/java/com/demo/api/MyApp.java
@@ -1,4 +1,13 @@
 package com.demo.api;
 
-public class MyApp {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class MyApp extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return super.getClasses();
+    }
 }
--- src/main/java/com/demo/api/ProductService.java
+++ src/main/java/com/demo/api/ProductService.java
@@ -1,4 +1,23 @@
 package com.demo.api;
 
+import com.demo.dao.ProductDAO;
+import com.demo.entity.Product;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+@Path("/products")
 public class ProductService {
+    @EJB
+    ProductDAO dao;
+
+    @GET
+    @Produces("application/json")
+    public List<Product> getProduct()
+    {
+        return dao.getProduct();
+    }
 }
